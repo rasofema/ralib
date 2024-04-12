5a8aed569e2a446e39cd189d44c670e28c7a7996
Raquel <raquel.silva.20@ucl.ac.uk>
2024-04-12 17:38:12 +0100

    Fixed doFindSeparatingWord to use BFS on tree for simulation only

--- src/main/java/de/learnlib/ralib/automata/tree/AbstractRATreeBuilder.java
+++ src/main/java/de/learnlib/ralib/automata/tree/AbstractRATreeBuilder.java
@@ -18,12 +18,10 @@ package de.learnlib.ralib.automata.tree;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.Iterator;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import de.learnlib.ralib.automata.RALocation;
 import de.learnlib.ralib.automata.RegisterAutomaton;
 import de.learnlib.ralib.words.PSymbolInstance;
 import net.automatalib.alphabet.Alphabet;
@@ -36,7 +34,6 @@ import net.automatalib.incremental.dfa.Acceptance;
 import net.automatalib.ts.UniversalDTS;
 import net.automatalib.util.ts.traversal.TSTraversal;
 import net.automatalib.word.Word;
-import net.automatalib.word.WordBuilder;
 
 abstract class AbstractRATreeBuilder implements RABuilder {
 
@@ -52,65 +49,39 @@ abstract class AbstractRATreeBuilder implements RABuilder {
     public @Nullable Word<PSymbolInstance> findSeparatingWord(RegisterAutomaton target,
                                                               Collection<? extends PSymbolInstance> inputs,
                                                               boolean omitUndefined) {
-        return doFindSeparatingWord(target, inputs, omitUndefined);
+        if (!omitUndefined) {
+            throw new UnsupportedOperationException("Cannot perform bisimulation, there may be more than one transition per symbol in an RA.");
+        }
+        return doFindSeparatingWord(target, inputs);
     }
 
     <S> @Nullable Word<PSymbolInstance> doFindSeparatingWord(RegisterAutomaton target,
-                                               Collection<? extends PSymbolInstance> inputs,
-                                               boolean omitUndefined) {
-        RALocation automatonInit = target.getInitialState();
-
-        if (automatonInit == null) {
-            return omitUndefined ? null : Word.epsilon();
+                                                             Collection<? extends PSymbolInstance> alphabet) {
+        if (target.getInitialState() == null) {
+            return null;
         }
 
-        if (root.getAcceptance().conflicts(automatonInit.isAccepting())) {
-            return Word.epsilon();
-        }
+        Deque<Pair<Node,Word<PSymbolInstance>>> bfsStack = new ArrayDeque<>();
+        bfsStack.push(Pair.of(this.root,Word.epsilon()));
 
-        // incomingInput can be null here, because we will always skip the bottom stack element below
-        @SuppressWarnings("nullness")
-        Record init = new Record(automatonInit, root, null,
-                inputs.iterator());
 
-        Deque<Record> dfsStack = new ArrayDeque<>();
-        dfsStack.push(init);
-
-        while (!dfsStack.isEmpty()) {
+        while (!bfsStack.isEmpty()) {
             @SuppressWarnings("nullness") // false positive https://github.com/typetools/checker-framework/issues/399
-            @NonNull Record rec = dfsStack.peek();
-            if (!rec.inputIt.hasNext()) {
-                dfsStack.pop();
-                continue;
-            }
-            PSymbolInstance input = rec.inputIt.next();
+            @NonNull Pair<Node,Word<PSymbolInstance>> pair = bfsStack.pop();
 
-            Node succ = rec.treeNode.getChild(input);
-            if (succ == null) {
-                continue;
+            Node node = pair.getFirst();
+            Word<PSymbolInstance> input = pair.getSecond();
+
+            if (node.getAcceptance().conflicts(target.accepts(input))) {
+                return input;
             }
 
-            @Nullable RALocation state = rec.automatonState;
-            @Nullable RALocation automatonSucc = state == null ? null : target.getTransition(state, input).getDestination();
-            if (automatonSucc == null && omitUndefined) {
-                continue;
-            }
-
-            boolean succAcc = automatonSucc != null && automatonSucc.isAccepting();
-
-            if (succ.getAcceptance().conflicts(succAcc)) {
-                WordBuilder<PSymbolInstance> wb = new WordBuilder<>(dfsStack.size());
-                wb.append(input);
-
-                dfsStack.pop();
-                while (!dfsStack.isEmpty()) {
-                    wb.append(rec.incomingInput);
-                    rec = dfsStack.pop();
+            for (PSymbolInstance in : alphabet) {
+                Node child = node.getChild(in);
+                if (child != null) {
+                    bfsStack.push(Pair.of(child,input.append(in)));
                 }
-                return wb.reverse().toWord();
             }
-
-            dfsStack.push(new Record(automatonSucc, succ, input, inputs.iterator()));
         }
 
         return null;
@@ -138,22 +109,6 @@ abstract class AbstractRATreeBuilder implements RABuilder {
     abstract Node createNode();
 
     abstract Node insertNode(Node parent, PSymbolInstance symIdx, Boolean accept);
-
-    static final class Record {
-
-        public final RALocation automatonState;
-        public final Node treeNode;
-        public final PSymbolInstance incomingInput;
-        public final Iterator<? extends PSymbolInstance> inputIt;
-
-        Record(RALocation automatonState, Node treeNode, PSymbolInstance incomingInput,
-               Iterator<? extends PSymbolInstance> inputIt) {
-            this.automatonState = automatonState;
-            this.treeNode = treeNode;
-            this.incomingInput = incomingInput;
-            this.inputIt = inputIt;
-        }
-    }
 
     class TransitionSystemViewAccept implements UniversalDTS<Node, PSymbolInstance, Node, Acceptance, Void>,
             UniversalAutomaton<Node, PSymbolInstance, Node, Acceptance, Void> {
